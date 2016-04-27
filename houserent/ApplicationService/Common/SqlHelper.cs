eff4145338642f35dddef742baebc57eaba04482
LawrenceDingYong <acause8023@hotmail.com>
2016-04-28 02:37:43 +0800

    添加业务逻辑层

---
+++ houserent/ApplicationService/Common/SqlHelper.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationService.Common
+{
+    public class SqlHelper
+    {
+        public SqlHelper()
+        {
+        }
+
+        public static readonly string CONN_STRING = System.Configuration.ConfigurationManager.ConnectionStrings["DBLink"].ConnectionString;
+        private static Hashtable parmCache = Hashtable.Synchronized(new Hashtable());
+
+        private static void PrepareCommand(SqlCommand cmd, SqlConnection conn, SqlTransaction trans, CommandType cmdType, string cmdText, SqlParameter[] cmdParms)
+        {
+            if (conn.State != ConnectionState.Open)
+            {
+                conn.Open();
+            }
+            cmd.Connection = conn;
+            cmd.CommandText = cmdText;
+            if (trans != null)
+            {
+                cmd.Transaction = trans;
+            }
+            cmd.CommandType = cmdType;
+            //cmd
+            if (cmdParms != null)
+            {
+                foreach (SqlParameter parm in cmdParms)
+                {
+                    cmd.Parameters.Add(parm);
+                }
+            }
+        }
+
+
+        /// Execute a SqlCommand (that returns no resultset) against the database specified in the connection string 
+        /// int result = ExecuteNonQuery(connString, CommandType.StoredProcedure, "PublishOrders", new SqlParameter("@prodid", 24));
+        public static int ExecuteNonQuery(string connString, CommandType cmdType, string cmdText, params SqlParameter[] cmdParms)
+        {
+
+            SqlCommand cmd = new SqlCommand();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                PrepareCommand(cmd, conn, null, cmdType, cmdText, cmdParms);
+                int val = cmd.ExecuteNonQuery();
+                //Clear cmd Parameters
+                cmd.Parameters.Clear();
+
+                conn.Close();
+                return val;
+            }
+        }
+
+
+        /// Execute a SqlCommand (that returns no resultset) against an existing database connection 
+        /// using the provided parameters.
+        ///  int result = ExecuteNonQuery(connString, CommandType.StoredProcedure, "PublishOrders", new SqlParameter("@prodid", 24));
+
+        public static int ExecuteNonQuery(SqlConnection conn, CommandType cmdType, string cmdText, params SqlParameter[] cmdParms)
+        {
+
+            SqlCommand cmd = new SqlCommand();
+            PrepareCommand(cmd, conn, null, cmdType, cmdText, cmdParms);
+            int val = cmd.ExecuteNonQuery();
+            cmd.Parameters.Clear();
+            conn.Close();
+            return val;
+        }
+
+
+        ///  int result = ExecuteNonQuery(connString, CommandType.StoredProcedure, "PublishOrders", new SqlParameter("@prodid", 24));
+        public static int ExecuteNonQuery(SqlTransaction trans, CommandType cmdType, string cmdText, params SqlParameter[] cmdParms)
+        {
+            SqlCommand cmd = new SqlCommand();
+            PrepareCommand(cmd, trans.Connection, trans, cmdType, cmdText, cmdParms);
+            int val = cmd.ExecuteNonQuery();
+            cmd.Parameters.Clear();
+
+            return val;
+        }
+
+
+        /// Execute a SqlCommand (that returns a resultset and takes no parameters) against the database specified in  the connection string. 
+        ///  SqlDataReader dr = ExecuteReader(connString, CommandType.StoredProcedure, "GetOrders");
+        public static SqlDataReader ExecuteReader(string connectionString, CommandType commandType, string commandText)
+        {
+            return ExecuteReader(connectionString, commandType, commandText, (SqlParameter[])null);
+        }
+
+
+        /// Execute a SqlCommand that returns a resultset against the database specified in the connection string 
+        /// using the provided parameters.
+        ///  SqlDataReader r = ExecuteReader(connString, CommandType.StoredProcedure, "PublishOrders", new SqlParameter("@prodid", 24));
+
+        public static SqlDataReader ExecuteReader(string connString, CommandType cmdType, string cmdText, params SqlParameter[] cmdParms)
+        {
+            SqlCommand cmd = new SqlCommand();
+            SqlConnection conn = new SqlConnection(connString);
+
+            try
+            {
+                PrepareCommand(cmd, conn, null, cmdType, cmdText, cmdParms);
+                SqlDataReader rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                cmd.Parameters.Clear();
+                return rdr;
+            }
+            catch
+            {
+                conn.Close();
+                throw;
+            }
+        }
+
+
+        /// <summary>
+        /// Execute a SqlCommand (that returns a resultset and takes no parameters) against the database specified in 
+        /// the connection string. 
+        /// e.g.:  
+        ///  DataSet ds = ExecuteDataset(connString, CommandType.StoredProcedure, "GetOrders");
+        public static DataSet ExecuteDataset(string connectionString, CommandType commandType, string commandText)
+        {
+            return ExecuteDataset(connectionString, commandType, commandText, (SqlParameter[])null);
+        }
+
+
+        /// <summary>
+        /// Execute a SqlCommand (that returns a resultset) against the database specified in the connection string 
+        /// using the provided parameters.
+        /// </summary>
+        /// <remarks>
+        /// e.g.:  
+        ///  DataSet ds = ExecuteDataset(connString, CommandType.StoredProcedure, "GetOrders", new SqlParameter("@prodid", 24));
+        /// </remarks>
+        /// <param name="connectionString">a valid connection string for a SqlConnection</param>
+        /// <param name="commandType">the CommandType (stored procedure, text, etc.)</param>
+        /// <param name="commandText">the stored procedure name or T-SQL command</param>
+        /// <param name="commandParameters">an array of SqlParamters used to execute the command</param>
+        /// <returns>a dataset containing the resultset generated by the command</returns>
+        public static DataSet ExecuteDataset(string connectionString, CommandType commandType, string commandText, params SqlParameter[] commandParameters)
+        {
+
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            {
+                cn.Open();
+                return ExecuteDataset(cn, commandType, commandText, commandParameters);
+            }
+        }
+
+
+        /// <summary>
+        /// Execute a SqlCommand (that returns a resultset and takes no parameters) against the provided SqlConnection. 
+        /// </summary>
+        /// <remarks>
+        /// e.g.:  
+        ///  DataSet ds = ExecuteDataset(conn, CommandType.StoredProcedure, "GetOrders");
+        /// </remarks>
+        /// <param name="connection">a valid SqlConnection</param>
+        /// <param name="commandType">the CommandType (stored procedure, text, etc.)</param>
+        /// <param name="commandText">the stored procedure name or T-SQL command</param>
+        /// <returns>a dataset containing the resultset generated by the command</returns>
+        public static DataSet ExecuteDataset(SqlConnection connection, CommandType commandType, string commandText)
+        {
+            return ExecuteDataset(connection, commandType, commandText, (SqlParameter[])null);
+        }
+
+
+        /// <summary>
+        /// Execute a SqlCommand (that returns a resultset) against the specified SqlConnection 
+        /// using the provided parameters.
+        /// </summary>
+        /// <remarks>
+        /// e.g.:  
+        ///  DataSet ds = ExecuteDataset(conn, CommandType.StoredProcedure, "GetOrders", new SqlParameter("@prodid", 24));
+        /// </remarks>
+        /// <param name="connection">a valid SqlConnection</param>
+        /// <param name="commandType">the CommandType (stored procedure, text, etc.)</param>
+        /// <param name="commandText">the stored procedure name or T-SQL command</param>
+        /// <param name="commandParameters">an array of SqlParamters used to execute the command</param>
+        /// <returns>a dataset containing the resultset generated by the command</returns>
+        public static DataSet ExecuteDataset(SqlConnection connection, CommandType commandType, string commandText, params SqlParameter[] commandParameters)
+        {
+
+
+            SqlCommand cmd = new SqlCommand();
+            PrepareCommand(cmd, connection, (SqlTransaction)null, commandType, commandText, commandParameters);
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+
+            da.Fill(ds);
+
+            cmd.Parameters.Clear();
+
+            return ds;
+        }
+
+
+        /// <summary>
+        /// Execute a SqlCommand that returns the first column of the first record against the database specified in the connection string 
+        /// using the provided parameters.
+        /// </summary>
+        /// <remarks>
+        /// e.g.:  
+        ///  Object obj = ExecuteScalar(connString, CommandType.StoredProcedure, "PublishOrders", new SqlParameter("@prodid", 24));
+        /// </remarks>
+        /// <param name="connectionString">a valid connection string for a SqlConnection</param>
+        /// <param name="commandType">the CommandType (stored procedure, text, etc.)</param>
+        /// <param name="commandText">the stored procedure name or T-SQL command</param>
+        /// <param name="commandParameters">an array of SqlParamters used to execute the command</param>
+        /// <returns>An object that should be converted to the expected type using Convert.To{Type}</returns>
+        public static object ExecuteScalar(string connString, CommandType cmdType, string cmdText, params SqlParameter[] cmdParms)
+        {
+            SqlCommand cmd = new SqlCommand();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                PrepareCommand(cmd, conn, null, cmdType, cmdText, cmdParms);
+                object val = cmd.ExecuteScalar();
+                cmd.Parameters.Clear();
+                return val;
+            }
+        }
+
+
+        /// <summary>
+        /// Execute a SqlCommand that returns the first column of the first record against an existing database connection 
+        /// using the provided parameters.
+        /// </summary>
+        /// <remarks>
+        /// e.g.:  
+        ///  Object obj = ExecuteScalar(connString, CommandType.StoredProcedure, "PublishOrders", new SqlParameter("@prodid", 24));
+        /// </remarks>
+        /// <param name="conn">an existing database connection</param>
+        /// <param name="commandType">the CommandType (stored procedure, text, etc.)</param>
+        /// <param name="commandText">the stored procedure name or T-SQL command</param>
+        /// <param name="commandParameters">an array of SqlParamters used to execute the command</param>
+        /// <returns>An object that should be converted to the expected type using Convert.To{Type}</returns>
+        public static object ExecuteScalar(SqlConnection conn, CommandType cmdType, string cmdText, params SqlParameter[] cmdParms)
+        {
+            SqlCommand cmd = new SqlCommand();
+            PrepareCommand(cmd, conn, null, cmdType, cmdText, cmdParms);
+            object val = cmd.ExecuteScalar();
+            cmd.Parameters.Clear();
+            return val;
+        }
+
+        /// <summary>
+        /// add parameter array to the cache
+        /// </summary>
+        /// <param name="cacheKey">Key to the parameter cache</param>
+        /// <param name="cmdParms">an array of SqlParamters to be cached</param>
+        public static void CacheParameters(string cacheKey, params SqlParameter[] cmdParms)
+        {
+            parmCache[cacheKey] = cmdParms;
+        }
+
+
+        /// <summary>
+        /// Retrieve cached parameters
+        /// </summary>
+        /// <param name="cacheKey">key used to lookup parameters</param>
+        /// <returns>Cached SqlParamters array</returns>
+        public static SqlParameter[] GetCachedParameters(string cacheKey)
+        {
+            SqlParameter[] cachedParms = (SqlParameter[])parmCache[cacheKey];
+
+            if (cachedParms == null)
+                return null;
+
+            SqlParameter[] clonedParms = new SqlParameter[cachedParms.Length];
+
+            for (int i = 0, j = cachedParms.Length; i < j; i++)
+                clonedParms[i] = (SqlParameter)((ICloneable)cachedParms[i]).Clone();
+
+            return clonedParms;
+        }
+
+    }
+}
